1e77c38dc521c04132f8de33ef7b46dabbdd1ded
0kalekale <kalekale.anon@gmail.com>
2022-02-11 14:21:35 +0545

    [meta] update TODO.org and update comments

--- include/sym.h
+++ include/sym.h
@@ -1,7 +1,7 @@
 /*
 	Multiple tentative defintions in different compilation units are not allowed in standard C.
-	TODO: Fix symtab definition with respect to standard C so that i can remove the -fcommon hack,    
-	"If you give me anymore trouble, I'll seal you up in bubble gum and send you flying off into space"
+	DONE(211489420e9f0fc49c58579b3de7e704cc47d97c): Fix symtab definition with respect to standard C so that i can remove the -fcommon hack    
+	"If you give me anymore trouble, I'll seal you up in bubble gum and send you flying off into space."
 */
 
 #define NSYMS 20	
